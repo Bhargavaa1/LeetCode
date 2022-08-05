fcb6032b2ed7f36e69002316b7b92af4e5afead1
Bhargavaa1 <abhargava@g.hmc.edu>
2022-08-04 22:42:13 -0400

    Added Comments to generateParenthesis

--- Recursion And Backtracking/generateParenthesis.py
+++ Recursion And Backtracking/generateParenthesis.py
@@ -1,16 +1,22 @@
+# https://leetcode.com/problems/generate-parentheses/
+# Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+# 22. Generate Parentheses
+
+# Enumerative Backtracking
 def generateParenthesis(self, n: int) -> List[str]:
     result = []
     stack = []
+
     def generateParenthesisHelper(openN: int, closedN: int):
         if closedN == openN == n:
             result.append("".join(stack))
         if openN < n:
             stack.append("(")
-            generateParenthesisHelper(openN+1,closedN)
+            generateParenthesisHelper(openN+1, closedN)
             stack.pop()
         if closedN < openN:
             stack.append(")")
-            generateParenthesisHelper(openN,closedN+1)
+            generateParenthesisHelper(openN, closedN+1)
             stack.pop()
-    generateParenthesisHelper(0,0)
+    generateParenthesisHelper(0, 0)
     return result
