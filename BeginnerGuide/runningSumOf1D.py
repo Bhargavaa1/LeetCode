e6e5af48453a1c36ddc43046e2fd3eaaf5fbcc22
Bhargavaa1 <abhargava@g.hmc.edu>
2022-08-31 11:25:01 -0700

    Running Sum of 1D Array In-Place Solution

--- BeginnerGuide/runningSumOf1D.py
+++ BeginnerGuide/runningSumOf1D.py
@@ -3,11 +3,20 @@ from typing import *
 # # Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
 # # Return the running sum of nums.
 class Solution:
-    # Caching
-    # Runtime: O(N) Space complexity: O(N)
-    def  runningSum(self, nums: List[int]) -> List[int]:
+    # Using "Extra" Space
+    # Runtime: O(N) Space complexity: O(1) [Input and Output Array Space does not count]
+    def runningSum(self, nums: List[int]) -> List[int]:
         result = [0]*len(nums)
         result[0] = nums[0]
         for i in range(1, len(nums)):
             result[i] = result[i-1]+nums[i]
         return result
+    
+    # In-place
+    # Runtime: O(N) Space complexity: O(1) [Input and Output Array Space does not count]
+    def runningSumInPlace(self, nums: List[int]) -> List[int]:
+        for i in range(1,len(nums)):
+            nums[i] = nums[i-1]+nums[i]
+        return nums            
+
+    
