f50ea0bf4c0e01ead7aecf51b1a7ded19c464343
Bhargavaa1 <abhargava@g.hmc.edu>
2022-09-11 23:59:51 -0700

    Dynamic Programming Solution for LCS

--- Dynamic Programming/longestCommonSubsequence.py
+++ Dynamic Programming/longestCommonSubsequence.py
@@ -8,14 +8,30 @@ from typing import *
 # A common subsequence of two strings is a subsequence that is common to both strings.
 
 class Solution:
+    # Recursive Solution
     def longestCommonSubsequenceRecursive(self, text1: str, text2: str) -> int:
         if not text1 or not text2:
             return 0
         elif text[-1] == text2[-1]:
-            return 1+self.longestCommonSubsequence(text1[:-1], text2[:-1])
+            return 1+self.longestCommonSubsequenceRecursive(text1[:-1], text2[:-1])
         else:
-            option1 = self.longestCommonSubsequence(text1, text2[:-1])
-            option2 = self.longestCommonSubsequence(text1[:-1], text2)
+            option1 = self.longestCommonSubsequenceRecursive(text1, text2[:-1])
+            option2 = self.longestCommonSubsequenceRecursive(text1[:-1], text2)
             return max(option1,option2)
     
-    # def longestCommonSubsequenceDP(self, text1: str, text2: str) -> int:
+    # Dynamic Programming Solution
+    def longestCommonSubsequenceDP(self, text1: str, text2: str) -> int:
+        m,n = len(text2), len(text1)
+        DP = [[0]*(n+1) for col in range(m+1)]
+        for col in range(n+1):
+            DP[0][col] = 0
+        for row in range(m+1):
+            DP[row][0] = 0
+        for row in range(1,m+1):
+            for col in range(1,n+1):
+                if text1[col-1] == text2[row-1]:
+                    DP[row][col] = 1+DP[row-1][col-1]
+                else:
+                    DP[row][col] = max(DP[row-1][col],DP[row][col-1])
+        return DP[m][n]
+
