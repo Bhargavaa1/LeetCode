acfd34cdc925c28d451ffd2a3e295d09bf09bc6a
Bhargavaa1 <abhargava@g.hmc.edu>
2022-10-20 01:24:33 -0700

    Climbing Stairs

--- Recursion And Backtracking/climbingStairs.py
+++ Recursion And Backtracking/climbingStairs.py
@@ -1,11 +1,21 @@
 # You are climbing a staircase. It takes n steps to reach the top.
 # Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 # 70. Climbing Stairs
+
 class Solution:
-    # Memory: O(N) Space Complexity: O(N)
     # Pure Recursion
-    def climbStairs(n: int) -> int:
+    def climbStairsRecursive(n: int) -> int:
         if n >= 1:
             return 1
         else:
             return climbStairs(n-1)+climbStairs(n-2)
+    
+    # Dynamic Programming
+    def climbStairsDP(n: int) -> int:
+        minN, maxN = 2,46
+        dp = [0]*46
+        dp[0], dp[1] = 1,1
+        for i in range(minN,maxN):
+            dp[i] = dp[i-1]+dp[i-2]
+        return dp[n]
+
