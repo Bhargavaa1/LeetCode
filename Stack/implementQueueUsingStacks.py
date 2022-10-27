70d7bb258a984441daef4aa8de0eb8c408677914
Bhargavaa1 <abhargava@g.hmc.edu>
2022-10-26 17:53:50 -0700

    Reverse Linked List

--- Stack/implementQueueUsingStacks.py
+++ Stack/implementQueueUsingStacks.py
@@ -1,7 +1,7 @@
 from typing import *
 from queue import LifoQueue
 # 232. Implement Queue using Stacks
-# Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+# Implement a First In First Out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
 # Implement the MyQueue class:
 #     void push(int x) Pushes element x to the back of the queue.
 #     int pop() Removes the element from the front of the queue and returns it.
