8c16e8dff7cc071dca97e5f2efa25d21c921fbca
Bhargavaa1 <abhargava@g.hmc.edu>
2024-10-07 22:43:19 -0400

    feat: implement remove duplicates

---
+++ TwoPointers/SameDirection/RemoveDuplicates.py
@@ -0,0 +1,14 @@
+# Given a sorted list of numbers, remove duplicates and return the new length. You must do this in-place and without using extra memory.
+# Input: [0, 0, 1, 1, 1, 2, 2]
+# Output: 3
+from typing import List
+
+
+def remove_duplicates(nums: List[int]) -> int:
+  slow = 0
+  for fast in range(nums):
+    if nums[slow] != nums[fast]:
+      slow += 1
+      nums[slow] = nums[fast]
+  return slow+1
+
