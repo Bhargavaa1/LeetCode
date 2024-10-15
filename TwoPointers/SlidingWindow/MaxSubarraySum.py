1b29c4049fa242e8b47a1f8e021f247a41ceb64e
Bhargavaa1 <abhargava@g.hmc.edu>
2024-10-15 16:18:30 -0400

    chore: fix typo

--- TwoPointers/SlidingWindow/MaxSubarraySum.py
+++ TwoPointers/SlidingWindow/MaxSubarraySum.py
@@ -1,6 +1,6 @@
 # Given an array (list) nums consisted of only non-negative integers, find the largest sum among all subarrays of length k in nums.
 # For example, if the input is nums = [1, 2, 3, 7, 4, 1], k = 3, then the output would be 14 as the largest length 3 subarray sum is given by [3, 7, 4] which sums to 14.
-# About subarrays or substrings -> Dealswith sums or additive -> Sliding Window
+# About subarrays or substrings -> Deals with sums or additive -> Sliding Window
 from typing import List
 
 
